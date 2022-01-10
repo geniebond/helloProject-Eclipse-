ae873eb4e002c0ded7caaed2fba2149740974d82
genie <guru0718@naver.com>
2022-01-10 11:41:48 +0900

    2022-01-10

---
+++ helloWorld/src/main/webapp/api/MOCA_DATA.js
@@ -0,0 +1,18 @@
+// MOCK_DATA.JSON
+
+
+let datas = `[{"id":1,"first_name":"Dwayne","last_name":"Blow","email":"dblow0@time.com","gender":"Male"},
+{"id":2,"first_name":"Dolph","last_name":"Tossell","email":"dtossell1@walmart.com","gender":"Female"},
+{"id":3,"first_name":"Ferdinande","last_name":"Medcraft","email":"fmedcraft2@trellian.com","gender":"Male"},
+{"id":4,"first_name":"Adrian","last_name":"Reyna","email":"areyna3@reuters.com","gender":"Female"},
+{"id":5,"first_name":"Bethina","last_name":"Jorat","email":"bjorat4@sfgate.com","gender":"Female"},
+{"id":6,"first_name":"Marti","last_name":"Cullip","email":"mcullip5@reddit.com","gender":"Female"},
+{"id":7,"first_name":"Valeria","last_name":"Stockill","email":"vstockill6@arstechnica.com","gender":"Female"},
+{"id":8,"first_name":"Sybil","last_name":"Melburg","email":"smelburg7@creativecommons.org","gender":"Female"},
+{"id":9,"first_name":"Sheila-kathryn","last_name":"Edelman","email":"sedelman8@vkontakte.ru","gender":"Female"},
+{"id":10,"first_name":"Dotty","last_name":"Parkman","email":"dparkman9@ox.ac.uk","gender":"Female"},
+{"id":11,"first_name":"Collie","last_name":"Starking","email":"cstarkinga@japanpost.jp","gender":"Male"},
+{"id":12,"first_name":"Nicoline","last_name":"Sacase","email":"nsacaseb@prweb.com","gender":"Female"},
+{"id":13,"first_name":"Barth","last_name":"Golborne","email":"bgolbornec@qq.com","gender":"Male"},
+{"id":14,"first_name":"Camille","last_name":"Denty","email":"cdentyd@privacy.gov.au","gender":"Female"},
+{"id":15,"first_name":"Hillie","last_name":"Gunderson","email":"hgundersone@prnewswire.com","gender":"Female"}]`
